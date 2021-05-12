69be1ee8763ffa3b6a2e0d287f7393e76ca6d1d4
Sandra Coburn <sandracoburn@ezs-MacBook-Pro.local>
2021-05-11 20:01:07 -0700

    Integrated firebase sign in into application, styled buttons for sign in page. Sign in with google.

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -16,16 +16,24 @@ class App extends React.Component {
       currentUser: null,
     };
   }
+
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
       this.setState({ currentUser: user });
       console.log('user', user);
     });
   }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
