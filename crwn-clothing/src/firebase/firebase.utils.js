54b5ae039b15f2e62f9a5b47a73bb699b4b5275c
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-05-12 19:47:18 -0700

    Fixed typo, added firebase code to add queries to database

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -14,6 +14,11 @@ const config = {
   measurementId: 'G-GQBBJT3ZPD',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  console.log(firestore.doc('users/128fdasyadu'));
+};
+
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
