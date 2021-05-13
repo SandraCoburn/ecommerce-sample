5b11e7a09cb3b351a9aabd27fb29710d6b6cf2f4
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-05-13 11:09:39 -0700

    Added a function for firestore to store the users data in the cloud database, google users get added to app state

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -20,10 +20,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user);
-      // this.setState({ currentUser: user });
-      console.log('user', user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          console.log('snapshot data', snapShot.data());
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log('this state', this.state);
+            }
+          );
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
