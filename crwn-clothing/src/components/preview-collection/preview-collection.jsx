a26fd1905a2c906442e67c4901e4d0d91dc8b9e8
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-05-26 10:03:08 -0700

    Added cart item reducer to add new item to state

--- crwn-clothing/src/components/preview-collection/preview-collection.jsx
+++ crwn-clothing/src/components/preview-collection/preview-collection.jsx
@@ -9,9 +9,9 @@ const CollectionPreview = ({ title, items }) => {
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
-          .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
+          .filter((item, idx) => idx < 4) //show only 4 items
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
