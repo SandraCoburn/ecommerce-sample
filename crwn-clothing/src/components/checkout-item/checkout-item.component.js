efdcf8956cc2948f4aee5f6dd709392983c2eacb
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-06-05 16:33:25 -0700

    Added functionality to add and remove items in checkout component

--- crwn-clothing/src/components/checkout-item/checkout-item.component.js
+++ crwn-clothing/src/components/checkout-item/checkout-item.component.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../state/cart/cart.actions';
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../state/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
   return (
     <div className="checkout-item">
@@ -11,7 +15,15 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img alt="item" src={imageUrl} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <span className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
@@ -21,5 +33,7 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 };
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 export default connect(null, mapDispatchToProps)(CheckoutItem);
