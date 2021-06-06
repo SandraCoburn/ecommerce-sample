5fb720260bdfa9696f233001f1d70fa80504d5f2
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-06-05 20:21:22 -0700

    Added Redux persist to save cart orders in local storage

--- crwn-clothing/src/state/root-reducer.js
+++ crwn-clothing/src/state/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+//local storage from redux persist
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+//we configure the reducer we want to persist: cart
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
